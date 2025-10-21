fb691301053780e41c7f5a8f49295fb5379bcecf
Raghib Haque <raghib.haque.2007@gmail.com>
2025-10-21 15:10:24 +0100

    new and improved case 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 import oshi.SystemInfo; // sys info importing
+import oshi.hardware.Baseboard;
 import oshi.hardware.CentralProcessor;
+import oshi.hardware.ComputerSystem;
 import oshi.software.os.InternetProtocolStats;
 import java.util.*; // for scanner
 import oshi.hardware.Firmware;
@@ -9,7 +11,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         SystemInfo si = new SystemInfo();
-        System.out.println("===MENU=== \n 1. Display OS. \n 2. Display TCPv4 Stats. \n 3. Display CPU Info \n 4.PLCHD");
+        System.out.println("===MENU===" +
+                " \n 1. Display OS." +
+                " \n 2. Display TCPv4 Stats. " +
+                "\n 3. Display CPU Info " +
+                "\n 4. Hardware Info");
         switch (sc.nextInt()) {
             case 1:
                 System.out.println("OS: " + si.getOperatingSystem());
@@ -29,9 +35,22 @@ public class Main {
                 System.out.println("CPU utilization: " + Arrays.toString(processor.getSystemCpuLoadTicks()));
                 break;
             case 4: // placeholder
-                Firmware firm = si.getHardware().getComputerSystem().getFirmware();
-                System.out.println(firm);
-                System.out.println("Name" + firm.getName());
+
+                ComputerSystem cs = si.getHardware().getComputerSystem();
+                Firmware firm = cs.getFirmware();
+                Baseboard base = cs.getBaseboard();
+
+                System.out.println("=== Firmware Information ===");
+                System.out.println("Name: " + firm.getName());
+                System.out.println("Version: " + firm.getVersion());
+                System.out.println("Manufacturer: " + firm.getManufacturer());
+                System.out.println("Release Date: " + firm.getReleaseDate());
+
+                System.out.println("\n=== Baseboard Information ===");
+                System.out.println("Manufacturer: " + base.getManufacturer());
+                System.out.println("Model: " + base.getModel());
+                System.out.println("Version: " + base.getVersion());
+                System.out.println("Serial Number: " + base.getSerialNumber());
                 break;
             default:
                 System.out.println("Invalid choice!");
