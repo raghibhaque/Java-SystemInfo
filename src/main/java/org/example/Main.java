77ca6e26aab2e5523c51e1db3b6d78f99db798c8
Raghib Haque <raghib.haque.2007@gmail.com>
2025-10-21 15:46:20 +0100

    v5 revamped case 1 , case 5 better functioning, using iterated for loops to give us a comprehensive ven.id of every device there

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 import oshi.SystemInfo; // sys info importing
-import oshi.hardware.Baseboard;
-import oshi.hardware.CentralProcessor;
-import oshi.hardware.ComputerSystem;
+import oshi.hardware.*;
 import oshi.software.os.InternetProtocolStats;
 import java.util.*; // for scanner
-import oshi.hardware.Firmware;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -17,13 +15,18 @@ public class Main {
 
 
             System.out.println("===MENU===" +
-                    " \n 1. Display OS." +
+                    " \n 1. Display SYSTEM INFO." +
                     " \n 2. Display TCPv4 Stats. " +
                     "\n 3. Display CPU Info " +
                     "\n 4. Hardware Info" +
                     "\n 5. USB Devices");
             switch (sc.nextInt()) {
                 case 1:
+                    ComputerSystem cs2 = si.getHardware().getComputerSystem();
+                    System.out.println("\n=== SYSTEM DETAILS ===");
+                    System.out.println("Manufacturer : " + cs2.getManufacturer());
+                    System.out.println("Model : " + cs2.getModel());
+                    System.out.println("Serial Number : " + cs2.getSerialNumber());
                     System.out.println("OS: " + si.getOperatingSystem());
                     System.out.println("Current Platform : " + SystemInfo.getCurrentPlatform());
                     break;
@@ -59,12 +62,33 @@ public class Main {
                     System.out.println("Version: " + base.getVersion());
                     System.out.println("Serial Number: " + base.getSerialNumber());
                     break;
+
                 case 5:
-                    si.getHardware().getUsbDevices(true);
-                    System.out.println("USB Devices - History of Connected: " + si.getHardware().getUsbDevices(true));
-                    System.out.println("USB Devices - DIRECTLY CONNECTED: " + si.getHardware().getUsbDevices(false));
-                    System.out.println("Serial Number : " + si.getHardware().getComputerSystem().getSerialNumber());
-                    break;
+                    System.out.println("=== USB DEVICE INFORMATION ===");
+
+                    // Gets full history of usb's ever connected since Boolean b is true
+                    List<UsbDevice> usbDevices = si.getHardware().getUsbDevices(true);
+                    System.out.println("USB Devices - Hierarchical (All Connected):");
+                    for (UsbDevice device : usbDevices) {
+                        System.out.println("-------------------------------------------------");
+                        System.out.println("Name : " + device.getName());
+                        System.out.println("Vendor : " + device.getVendor());
+                        System.out.println("Vendor ID : " + device.getVendorId());
+                        System.out.println("Product ID : " + device.getProductId());
+                        System.out.println("Serial Num : " + device.getSerialNumber());
+                    }
+
+                    // get only CURRENT connections
+                    System.out.println("\nUSB Devices - Directly Connected:");
+                    List<UsbDevice> topDevices = si.getHardware().getUsbDevices(false);
+                    for (UsbDevice device : topDevices) { // for every device in the list we print its own details
+                        System.out.println("-------------------------------------------------");
+                        System.out.println("Name : " + device.getName());
+                        System.out.println("Vendor : " + device.getVendor());
+                        System.out.println("Vendor ID : " + device.getVendorId());
+                        System.out.println("Product ID : " + device.getProductId());
+                    }
+                break;
                 default:
                     System.out.println("Invalid choice!");
             }
