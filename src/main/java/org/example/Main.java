b24a880fc273b0cbd63780fb29622e5a4daff17a
Raghib Haque <raghib.haque.2007@gmail.com>
2025-10-21 15:24:01 +0100

    added a loop and now goes through program continiously

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,50 +11,58 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         SystemInfo si = new SystemInfo();
-        System.out.println("===MENU===" +
-                " \n 1. Display OS." +
-                " \n 2. Display TCPv4 Stats. " +
-                "\n 3. Display CPU Info " +
-                "\n 4. Hardware Info");
-        switch (sc.nextInt()) {
-            case 1:
-                System.out.println("OS: " + si.getOperatingSystem());
-                break;
+        boolean isRunning = true;
 
-            case 2:
-                InternetProtocolStats ipStats = si.getOperatingSystem().getInternetProtocolStats();
-                System.out.println("TCPv4 Stats: " + ipStats.getTCPv4Stats());
-                break;
+        while (true) {
 
-            case 3:
-                CentralProcessor processor = si.getHardware().getProcessor();
 
-                System.out.println("Logical processor count: " + processor.getLogicalProcessorCount());
-                System.out.println("Physical core count: " + processor.getPhysicalProcessorCount());
-                System.out.println("Physical package count: " + processor.getPhysicalPackageCount());
-                System.out.println("CPU utilization: " + Arrays.toString(processor.getSystemCpuLoadTicks()));
-                break;
-            case 4: // placeholder
+            System.out.println("===MENU===" +
+                    " \n 1. Display OS." +
+                    " \n 2. Display TCPv4 Stats. " +
+                    "\n 3. Display CPU Info " +
+                    "\n 4. Hardware Info");
+            switch (sc.nextInt()) {
+                case 1:
+                    System.out.println("OS: " + si.getOperatingSystem());
+                    System.out.println("Current Platform : " + SystemInfo.getCurrentPlatform());
+                    break;
 
-                ComputerSystem cs = si.getHardware().getComputerSystem();
-                Firmware firm = cs.getFirmware();
-                Baseboard base = cs.getBaseboard();
+                case 2:
+                    InternetProtocolStats ipStats = si.getOperatingSystem().getInternetProtocolStats();
+                    System.out.println("TCPv4 Stats: " + ipStats.getTCPv4Stats());
+                    break;
 
-                System.out.println("=== Firmware Information ===");
-                System.out.println("Name: " + firm.getName());
-                System.out.println("Version: " + firm.getVersion());
-                System.out.println("Manufacturer: " + firm.getManufacturer());
-                System.out.println("Release Date: " + firm.getReleaseDate());
+                case 3:
+                    CentralProcessor processor = si.getHardware().getProcessor();
 
-                System.out.println("\n=== Baseboard Information ===");
-                System.out.println("Manufacturer: " + base.getManufacturer());
-                System.out.println("Model: " + base.getModel());
-                System.out.println("Version: " + base.getVersion());
-                System.out.println("Serial Number: " + base.getSerialNumber());
-                break;
-            default:
-                System.out.println("Invalid choice!");
+                    System.out.println("Logical processor count: " + processor.getLogicalProcessorCount());
+                    System.out.println("Physical core count: " + processor.getPhysicalProcessorCount());
+                    System.out.println("Physical package count: " + processor.getPhysicalPackageCount());
+                    System.out.println("CPU utilization: " + Arrays.toString(processor.getSystemCpuLoadTicks()));
+                    break;
+                case 4: // placeholder
+
+                    ComputerSystem cs = si.getHardware().getComputerSystem();
+                    Firmware firm = cs.getFirmware();
+                    Baseboard base = cs.getBaseboard();
+
+                    System.out.println("=== Firmware Information ===");
+                    System.out.println("Name: " + firm.getName());
+                    System.out.println("Version: " + firm.getVersion());
+                    System.out.println("Manufacturer: " + firm.getManufacturer());
+                    System.out.println("Release Date: " + firm.getReleaseDate());
+
+                    System.out.println("\n=== Baseboard Information ===");
+                    System.out.println("Manufacturer: " + base.getManufacturer());
+                    System.out.println("Model: " + base.getModel());
+                    System.out.println("Version: " + base.getVersion());
+                    System.out.println("Serial Number: " + base.getSerialNumber());
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            isRunning = !isRunning;
+            sc.nextLine();
         }
     }
 }
-
